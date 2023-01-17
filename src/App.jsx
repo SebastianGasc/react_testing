a213504619d3c801448627307b701fcd757c4735
Sebastian Gasc <sebastian.gasc1701@alumno.ubiobio.cl>
2023-01-17 14:41:39 -0300

    Creada calculadora

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React, {Fragment, useState, useRef} from 'react';
+export function App(){
+    
+
+    let numeroUno=useRef();
+    let numeroDos=useRef();
+    const botonSuma= () =>{
+        const num1 = parseInt(numeroUno.current.value);
+        const num2 = parseInt(numeroDos.current.value);
+        var suma = num1+num2;
+        console.log(suma);
+        
+        return suma;
+    };
+
+    const botonResta= () =>{
+        const num1 = parseInt(numeroUno.current.value);
+        const num2 = parseInt(numeroDos.current.value);
+        var resta = num1-num2;
+        console.log(resta);
+        
+        return resta;
+    };
+
+    const botonMultiplicar= () =>{
+        const num1 = parseInt(numeroUno.current.value);
+        const num2 = parseInt(numeroDos.current.value);
+        var mult = num1*num2;
+        console.log(mult);
+        
+        return mult;
+    };
+
+    const botonDividir= () =>{
+        const num1 = parseInt(numeroUno.current.value);
+        const num2 = parseInt(numeroDos.current.value);
+        var division = num1/num2;
+        console.log(division);
+        
+        return division;
+    };
+
+    return( 
+        
+        <Fragment>
+            <div><h1>Holaa</h1></div>
+            
+            <Fragment>
+                <h2> Adios </h2>
+                <input ref={numeroUno} type="text" placeholder="nueva task"></input>
+                <input ref={numeroDos} type="text" placeholder="nueva task"></input>
+                <button onClick={botonSuma}>+</button>
+                <button onClick={botonResta}>-</button>
+                <button onClick={botonMultiplicar}>*</button>
+                <button onClick={botonDividir}>/</button>
+            </Fragment>
+        </Fragment>
+
+    );
+
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom"
-
-ReactDOM.render(<div><h1>Hola que tal</h1></div>, document.getElementById("root"));
+import { App } from './App';
+ReactDOM.render(<App/>, document.getElementById("root"));
